b38eb25d29bf07f0e349d8e84558bbd935d95854
Sgame <Sgame@Sp>
2024-11-26 17:45:54 -0500

    Archivos de la tarea solo Cliente.java

---
+++ src/ejemplo1/Cliente.java
@@ -0,0 +1,18 @@
+package ejemplo1;
+
+/*
+Autor: Santiago Palencia
+Tema: Codigo de banco 
+*/
+
+public class Cliente {
+    private String nombre;
+
+    public Cliente(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
